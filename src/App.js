cb6b3a4f7512ed11215c6061b6b312c71b9b641b
jllee000 <jllee000@naver.com>
2024-11-13 13:01:27 +0900

    feat : QR 페이지 연동

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./components/Main/Main";
 import NavBar from "./components/Nav/Navbar";
 import Order from "./pages/Order/Order";
-
+import OrderPay from "./pages/Order/OrderPay";
 const App = () => {
   return (
     <BrowserRouter>
@@ -12,6 +12,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/order/" element={<Order />} />
+            <Route path="/pay" element={<OrderPay />} />
           </Routes>
           <NavBar />
         </div>
