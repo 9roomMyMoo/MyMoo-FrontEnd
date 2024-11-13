f2229f6b391cba912c93098094c4880ecc079a3a
jllee000 <jllee000@naver.com>
2024-11-13 22:47:07 +0900

    feat : 주문 페이지 플로우 마무리

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./components/Main/Main";
 import NavBar from "./components/Nav/Navbar";
 import Order from "./pages/Order/Order";
 import OrderPay from "./pages/Order/OrderPay";
+import OrderMenu from "./pages/Order/OrderMenu";
 const App = () => {
   return (
     <BrowserRouter>
@@ -11,6 +12,7 @@ const App = () => {
         <div className="app-main">
           <Routes>
             <Route path="/" element={<Main />} />
+            <Route path="/order/:place" element={<OrderMenu />} />
             <Route path="/order/" element={<Order />} />
             <Route path="/pay" element={<OrderPay />} />
           </Routes>
